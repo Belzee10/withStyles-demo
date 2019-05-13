14b2d6b92632b911c0231f052d026b69f4d59187
Ivan <belzee92@gmail.com>
2019-05-13 19:05:02 -0400

    create Box cmp

---
+++ src/components/Box.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Box = props => {
+  const { children, className, style } = props;
+  return (
+    <div className={className} style={style}>
+      {children}
+    </div>
+  );
+};
+
+Box.propTypes = {
+  /**
+   *  Component's children
+   */
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  /**
+   * Class applied to component
+   */
+  className: PropTypes.string,
+  /**
+   * Inline styles applied to component
+   */
+  style: PropTypes.objectOf(PropTypes.any)
+};
+
+Box.defaultProps = {
+  children: null,
+  className: "",
+  style: {}
+};
+
+export default Box;
